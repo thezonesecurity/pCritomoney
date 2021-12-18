f1c55d56db018e637e739c7733186e836537a4d2
Romario Martinez <kids6352@gmail.com>
2021-12-18 16:09:38 -0400

    app de cripto money

---
+++ src/colors/theme.tsx
@@ -0,0 +1,8 @@
+abstract class Mycolors2 {
+  public static primary = '#F0D9E7';
+  public static secondary = '#FF94CC';
+  public static three = '#A239EA';
+  public static four = '#F38BA0';
+  public static five = '#5C33F6';
+}
+export default Mycolors2;
